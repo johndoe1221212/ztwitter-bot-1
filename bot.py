1900267bab9087ab66d655c2abc2b77bc8d65093
johndoe1221212 <lmaowhy322@gmail.com>
2025-06-28 09:09:36 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,18 +2,21 @@ import requests
 import tweepy
 from bs4 import BeautifulSoup
 
-# Twitter credentials (Tweepy v1.1 and v2)
+# Twitter credentials (fill in your own)
 consumer_key = 'sK7YeAis3LbNSK2wRzqMFEXCK'
 consumer_secret = 'kTLn8HSHwMBdAO6PFArcJDRrDHgzxMK1fJ1RgPtsIuiJq1t5TL'
 access_token = '1938981766104072192-5aIFTs7EdXNEYMPMFVXSiDeRjVR366'
 access_token_secret = 'CE7KbykD8W9msVe8Brp16gIyKqCeuOrstfMLTqhgKNvET'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFlA2wEAAAAAoJZw3tvm3tTRmeSZg%2B9uro8ATro%3DI7O3Vmc3UnGwa4PR63aBU1GIZqlKevzZHyfzbMLXaMZY5s5wTf'
 
-# Tweepy OAuth1 for API v1.1 (media upload + tweet)
-auth = tweepy.OAuth1UserHandler(
-    consumer_key, consumer_secret,
-    access_token, access_token_secret
+# Initialize Tweepy Client (v2)
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
+    bearer_token=bearer_token
 )
-api = tweepy.API(auth)
 
 def download_image(filename='face.jpg'):
     headers = {
@@ -40,9 +43,27 @@ def download_image(filename='face.jpg'):
     else:
         raise Exception("Image URL not found on page")
 
+def upload_media_v1_1(image_path):
+    """
+    Upload media using Twitter's v1.1 media/upload endpoint.
+    This requires bearer token authentication.
+    """
+    url = "https://upload.twitter.com/1.1/media/upload.json"
+    headers = {
+        'Authorization': f'Bearer {bearer_token}'
+    }
+    with open(image_path, 'rb') as f:
+        files = {
+            'media': f
+        }
+        response = requests.post(url, headers=headers, files=files)
+    response.raise_for_status()
+    media_id = response.json()['media_id_string']
+    return media_id
+
 def post_image(image_path):
-    media = api.media_upload(image_path)
-    api.update_status(status="", media_ids=[media.media_id])
+    media_id = upload_media_v1_1(image_path)
+    client.create_tweet(media_ids=[media_id])
 
 def main():
     image_file = download_image()
