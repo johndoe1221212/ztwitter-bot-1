dbcefaf87d088bf3b3a757c9672f1da6a82c35c6
johndoe1221212 <lmaowhy322@gmail.com>
2025-06-28 08:32:28 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import requests
+import tweepy
+
+# Twitter credentials (Tweepy v2 client)
+consumer_key = 'sK7YeAis3LbNSK2wRzqMFEXCK'
+consumer_secret = 'kTLn8HSHwMBdAO6PFArcJDRrDHgzxMK1fJ1RgPtsIuiJq1t5TL'
+access_token = '1938981766104072192-5aIFTs7EdXNEYMPMFVXSiDeRjVR366'
+access_token_secret = 'CE7KbykD8W9msVe8Brp16gIyKqCeuOrstfMLTqhgKNvET'
+
+# Initialize Tweepy Client (v2)
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
+def download_image(url='https://thispersondoesnotexist.com/image', filename='face.jpg'):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    return filename
+
+def post_image(image_path):
+    media = client.media_upload(image_path)
+    client.create_tweet(media_ids=[media.media_id])
+
+def main():
+    image_file = download_image()
+    post_image(image_file)
+
+if __name__ == '__main__':
+    main()
