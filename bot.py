ff83341d55979fb764232bcc7d849f2fc01a25f0
johndoe1221212 <lmaowhy322@gmail.com>
2025-06-28 08:53:52 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import requests
 import tweepy
+from bs4 import BeautifulSoup
 
 # Twitter credentials (Tweepy v2 client)
 consumer_key = 'sK7YeAis3LbNSK2wRzqMFEXCK'
@@ -15,17 +16,30 @@ client = tweepy.Client(
     access_token_secret=access_token_secret
 )
 
-def download_image(url='https://this-person-does-not-exist.com/en/image', filename='face.jpg'):
+def download_image(filename='face.jpg'):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                       "AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/114.0.0.0 Safari/537.36"
     }
-    response = requests.get(url, headers=headers)
+    page_url = 'https://this-person-does-not-exist.com/en'
+    response = requests.get(page_url, headers=headers)
     response.raise_for_status()
-    with open(filename, 'wb') as f:
-        f.write(response.content)
-    return filename
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    img_tag = soup.find('img')
+    if img_tag and 'src' in img_tag.attrs:
+        image_url = img_tag['src']
+        if image_url.startswith('/'):
+            image_url = 'https://this-person-does-not-exist.com' + image_url
+
+        img_response = requests.get(image_url, headers=headers)
+        img_response.raise_for_status()
+        with open(filename, 'wb') as f:
+            f.write(img_response.content)
+        return filename
+    else:
+        raise Exception("Image URL not found on page")
 
 def post_image(image_path):
     media = client.media_upload(image_path)
@@ -37,4 +51,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
